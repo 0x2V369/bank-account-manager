b7f9bf171d96594923bd3ec76e8e3a486585c440
0x2V <0x2V369@gmail.com>
2025-02-19 21:47:36 +0200

    Updated customer.py

--- customer.py
+++ customer.py
@@ -1,3 +1,6 @@
+from account import Account
+
+
 class Customer:
     def __init__(self, unique_customer_id, customer_name, customer_address=""):
         self.customer_id = unique_customer_id
@@ -6,10 +9,15 @@ class Customer:
         self.customer_accounts = []  # list of customer account ids
 
     def get_customer_information(self):
-        pass
+        """Return Customer information"""
+        print(f"Customer ID: {self.customer_id}")
+        print(f"Customer Name: {self.customer_name}")
 
-    def update_customer_detaisl(self):
+
+    def update_customer_details(self):
         pass
 
     def add_customer_account(self):
-        pass
+        """Create and add new account"""
+        new_account = Account(self.customer_id)
+        self.customer_accounts.append(new_account.account_number)
