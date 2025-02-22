8b85b0a16150bb66fe236a110476d2f670574ff8
0x2V <0x2V369@gmail.com>
2025-02-22 19:20:47 +0200

    Updated transaction.py methods

--- transaction.py
+++ transaction.py
@@ -6,8 +6,15 @@ class Transaction:
         self.transaction_id = 0
         self.account_number = account_number
         self.transaction_type = transaction_type
-        self.amount = amount
+        self.tx_amount = amount
         self.timestamp = datetime.utcnow()
+        self.transaction_details = {
+            "tx_id": self.transaction_id,
+            "account_number": self.account_number,
+            "tx_type": self.transaction_type,
+            "tx_amount": self.tx_amount,
+            "tx_timestamp": self.timestamp,
+        }
 
     def get_transaction_details(self, transaction_id):
         pass
